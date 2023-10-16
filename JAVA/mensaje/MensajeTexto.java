fa20720f1914cd93eee1b26c36d9a9726aef468e
PayoCentral <ph836854@gmail.com>
2023-10-16 16:50:05 -0600

    Ya jaló el 6

--- JAVA/mensaje/MensajeTexto.java
+++ JAVA/mensaje/MensajeTexto.java
@@ -1,31 +1,33 @@
-public class MensajeTexto {
+class MensajeTexto {
     private String code;
     private String mensaje;
-    
-    public MensajeTexto(String c, String m){
-        code=c;
-        mensaje=m;
+
+    public MensajeTexto(String c, String m) {
+        code = c;
+        mensaje = m;
     }
-    
+
     public String getCode() {
         return code;
     }
-    public String getMensaje(){
+
+    public String getMensaje() {
         return mensaje;
     }
-    
-    public void setCode(String code){
-        this.code=code;
+
+    public void setCode(String c1) {
+        code = c1;
     }
-        public void setMensaje(String mensaje){
-        this.mensaje=mensaje;
+
+    public void setMensaje(String m1) {
+        mensaje = m1;
     }
-    
-    public static void imprimeInfo(MensajeTexto m1){
-        if(setCode.equals("tqc")){
-            System.out.println(setMensaje);
+
+    public static void imprimeInfo(MensajeTexto m2) {
+        if (m2.getCode().equals("tqc")) {
+            System.out.println(m2.getMensaje());
         } else {
-            System.out.println(setCode);
+            System.out.println(m2.getCode());
         }
     }
 }
--- JAVA/mensaje/Principal.java
+++ JAVA/mensaje/Principal.java
@@ -1,21 +1,15 @@
 import java.util.Scanner;
-
 public class Principal{
 public static void main(String[] args){
     Scanner codigo = new Scanner(System.in);
     System.out.println("Digíte su código");
     String codigo1 = codigo.nextLine();
-    MensajeTexto.setCode(codigo1);
+    MensajeTexto mensajefinal = new MensajeTexto(codigo1, " ");
+    mensajefinal.setCode(codigo1);
     String mensaje1 = "Te quiero, cariño";
-    MensajeTexto.setMensaje(mensaje1);
-//    if(codigo1.equals("tqc")){
-//        System.out.println("Te quiero, cariño.");
-//    } else {
-//        System.out.println(codigo1);
-//    }
-//}
+    mensajefinal.setMensaje(mensaje1);
 
-MensajeTexto mensajefinal = new MensajeTexto(codigo1, mensaje1);
+
 MensajeTexto.imprimeInfo(mensajefinal);
 }
 }
