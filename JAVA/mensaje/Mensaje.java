ea1b93062b8c64a402322950a930165f71265afb
PayoCentral <ph836854@gmail.com>
2023-10-16 19:34:35 -0600

    Lissto

---
+++ JAVA/mensaje/Mensaje.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Mensaje extends MensajeTexto{
+    private String numllamada;
+    private String numdestinatario;
+    
+    public Mensaje(){
+        this("","","","");
+    }
+    
+    public Mensaje(String code, String mensaje, String numllamada, String numdestinatario){
+        super(code, mensaje);
+        this.numllamada=numllamada;
+        this.numdestinatario=numdestinatario;
+    }
+    public Mensaje(MensajeTexto mensajeTexto, String numllamada) {
+        this(mensajeTexto, null, numllamada);
+    }
+
+    public Mensaje(MensajeTexto mensajeTexto) {
+        this(mensajeTexto, null, null);
+    }
+
+    public String getNumllamada(){
+        return numllamada;
+    }
+    public String getNumdestinatario(){
+        return numdestinatario;
+    }
+    public String setNumllamada(String a1){
+        numllamada=a1;
+    }
+    public String setNumdestinatario(String a2){
+        numllamada=a2;
+    }
+    
+     public static void imprimeMensajeNum(Mensaje k1) {
+        System.out.println("Código: " + k1.getCode());
+        System.out.println("Mensaje: " + k1.getMensaje());
+        System.out.println("Número de llamada: " + k1.getNumllamada());
+        System.out.println("Número del remitente: " + k1.getNumdestinatario());
+    }
+}
--- JAVA/mensaje/Principal.java
+++ JAVA/mensaje/Principal.java
@@ -4,12 +4,21 @@ public static void main(String[] args){
     Scanner codigo = new Scanner(System.in);
     System.out.println("Digíte su código");
     String codigo1 = codigo.nextLine();
-    MensajeTexto mensajefinal = new MensajeTexto(codigo1, " ");
+    MensajeTexto mensajefinal = new MensajeTexto(codigo1, "");
     mensajefinal.setCode(codigo1);
     String mensaje1 = "Te quiero, cariño";
     mensajefinal.setMensaje(mensaje1);
 
 
 MensajeTexto.imprimeInfo(mensajefinal);
+
+System.out.println("Digite su número");
+String numero1s = codigo.nextLine();
+System.out.println("Digite el número del destinatario");
+String numero2s = codigo.nextLine();
+System.out.println("Cuál es su mensaje");
+String mensaje2s = codigo.nextLine();
+Mensaje mensajecon = new Mensaje(codigo1,mensaje2s, numero1s, numero2s);
+Mensaje.imprimeMensajeNum(mensajecon);
 }
 }
