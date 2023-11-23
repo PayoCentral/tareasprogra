2123b4e94bb7404f0ff1e7fa90ee4734e60f6134
PayoCentral <ph836854@gmail.com>
2023-11-23 07:33:10 -0600

    Create PrincipalTrabajador_clase.java

---
+++ JAVA/lab/PrincipalTrabajador_clase.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package principaltrabajador
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Luz A
+ */
+public class PrincipalTrabajador {
+  public static void main(String[] args) {
+  	Trabajador[] trabajador = new Trabajador[5];;
+    //Trabajador trabajador[] = new Trabajador[5];
+    int opc,i,num=0;
+    int continuar;
+      
+    do{
+       	// Creación de objetos
+       do{
+              
+          String op = JOptionPane.showInputDialog(" 1 :Empleado" + "\n" + " 2 :Consultor");
+          opc = Integer.parseInt(op);
+          switch(opc){
+		   	case 1:
+			trabajador [num]= new Empleado (JOptionPane.showInputDialog("Nombre"),JOptionPane.showInputDialog("Seguro"),Double.parseDouble (JOptionPane.showInputDialog("Sueldo")));
+            break;
+		   	case 2:
+		   	trabajador [num]= new Consultor (JOptionPane.showInputDialog("Nombre"),JOptionPane.showInputDialog("Seguro"),Integer.parseInt(JOptionPane.showInputDialog("Horas")),Double.parseDouble(JOptionPane.showInputDialog("Tarifa")));
+		    break;
+		   	    default:
+		   	     JOptionPane.showMessageDialog(null,  "Opción Incorrecta");
+
+		   }
+
+		   }while((opc < 1) && (opc > 2));
+		   num=num+1;
+		   
+           continuar= JOptionPane.showConfirmDialog(null,"¿Desea añadir otro Trabajador?","Recabando información",JOptionPane.YES_NO_OPTION);
+
+          }while(continuar==JOptionPane.YES_OPTION);
+         
+           for (i=0;i<num;i++)
+		       {
+				//Escribir Trabajadores
+                System.out.print(trabajador[i]);
+				System.out.println(trabajador[i].calcularPago());
+
+		      }
+	}
+}
